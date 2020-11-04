96cc44cf32af2fa851ad4069878b269f32be92c1
luisop <luisfop77@gmail.com>
2020-11-04 00:22:44 -0300

    started beerspage styling

--- src/pages/Beers/BeersPage.tsx
+++ src/pages/Beers/BeersPage.tsx
@@ -1,57 +1,57 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import './BeersPage.css';
+import "./BeersPage.css";
 
-import {Beers} from '../../mock/beers';
-import {BeerType} from '../../types/BeerType';
-import TextField from '@material-ui/core/TextField';
-import CardComponent from '../../components/CardComponent/CardComponent';
+import { Beers } from "../../mock/beers";
+import { BeerType } from "../../types/BeerType";
+import TextField from "@material-ui/core/TextField";
+import CardComponent from "../../components/CardComponent/CardComponent";
 
 const BeersPage = () => {
+  const [products, setProducts] = useState<BeerType[]>([]);
+  const [filterProduct, setFilterProduct] = useState<string>("");
 
-    const [products, setProducts] = useState<BeerType[]>([])
-    const [filterProduct, setFilterProduct] = useState<string>('');
+  useEffect(() => {
+    const getBeers = (): void => {
+      setProducts(Beers);
+    };
+    getBeers();
+  }, []);
 
-    useEffect(() => {
-        const getBeers = ():void => {
-            setProducts(Beers)
-        }
-        getBeers();
-    },[])    
-
-    const productCards = products
+  const productCards = products
     .filter(product => {
-        return product.name.toLowerCase().indexOf(filterProduct.toLowerCase()) >= 0
+      return (
+        product.name.toLowerCase().indexOf(filterProduct.toLowerCase()) >= 0
+      );
     })
     .map(product => (
-        <CardComponent
+      <CardComponent
         name={product.name}
         description={product.description}
         alcool={product.alcool}
         image={product.image}
         ibu={product.ibu}
         price={product.price}
+      />
+    ));
+
+  return (
+    <div className="beers--page">
+      <h1 className="title">Beers page!!!</h1>
+      <form className="search">
+        <TextField
+          label="xablau"
+          variant="filled"
+          className="form--input"
+          onChange={e => setFilterProduct(e.target.value)}
         />
-    ))
-    
+      </form>
 
-    const filtering = products.filter(xablau => {
-        return 
-    })
-
-    console.log(filterProduct);
-
-    return (
-        <div>
-            <h1 style={{color: 'white'}}>Beers page!!!</h1>
-            <form>
-                <TextField label="xablau" variant="outlined" className="form--input" onChange={(e) => setFilterProduct(e.target.value)}/>
-            </form>
-
-            {productCards}
-            {/* {filtering} */}
-        </div>
-    )
-}
+      <section className="beer--display">
+        <div className="display">{productCards}</div>
+      </section>
+    </div>
+  );
+};
 
 export default BeersPage;
